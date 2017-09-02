d283bae12e6d668bdceb4f6d3c054a07123fe59a
Matt Hensley <matt.hensley777@gmail.com>
2017-09-02 10:22:54 -0500

    es6 changes

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-var allProducts = [];
+const allProducts = [];
 
-var product1 = {
+const product1 = {
     name: "Mop Attire",
     imagePath: "./images/mopAttire.jpg",
     imageAlt: "Product: Mop Attire",
@@ -9,7 +9,7 @@ var product1 = {
     soldOut: false
 };
 
-var product2 = {
+const product2 = {
     name: "Taco Suit",
     imagePath: "./images/tacoSuit.jpg",
     imageAlt: "Product: Taco Suit",
@@ -18,7 +18,7 @@ var product2 = {
     soldOut: false
 };
 
-var product3 = {
+const product3 = {
     name: "Neck Decoration",
     imagePath: "./images/neckDecoration.jpg",
     imageAlt: "Product: Neck Decoration",
@@ -27,7 +27,7 @@ var product3 = {
     soldOut: false
 };
 
-var product4 = {
+const product4 = {
     name: "Head Ornament",
     imagePath: "./images/headOrnament.jpg",
     imageAlt: "Product: Head Ornament",
@@ -36,7 +36,7 @@ var product4 = {
     soldOut: true
 };
 
-var product5 = {
+const product5 = {
     name: "Boob Hat",
     imagePath: "./images/boobHat.jpg",
     imageAlt: "Product: Boob Hat",
@@ -52,18 +52,19 @@ allProducts.push(product3)
 allProducts.push(product4)
 allProducts.push(product5)
 
+const addNewProduct = (name, imagePath, imageAlt, description, price, soldOut) => {
+    const newProduct= {name, imagePath, imageAlt, description, price, soldOut};
+    allProducts.push(newProduct);
+}
+
+addNewProduct("Butt Cream", "https://upload.wikimedia.org/wikipedia/en/e/e8/Boudreauxs_Butt_Paste.jpg", "Here is a picture of butt cream.", "lorum oh well", 4.99, true);
 
 console.log(allProducts);
 
-var productContainer = document.getElementById("product-container");
+let productContainer = document.getElementById("product-container");
 
-
-
-
-
-
-function buildDomString(product) {
-    var domString = "";
+const buildDomString = (product) => {
+    let domString = "";
 
     domString += '<section class="product">';
     domString += '<div class="title">';
@@ -76,30 +77,52 @@ function buildDomString(product) {
     domString += '<p>' + product.description + '</p>';
     domString += '<h6>$' + product.price + '</h6>';
     domString += '</div>';
-    // domString += '</div>';
+    if ( product.soldOut) {
+      domString +=  '<div class="sold-out">'
+      domString +=  '<img src="./images/soldOut.png" alt="Sold Out">'
+      domString +=  '</div>'
+    }
     domString += '</section>';
     return domString;
 
     console.log("Dom String from for loop", domString);
 }
 
-function printProductArrayToDom(productArray) {
-  for (var i = 0; i < productArray.length; i++) {
+const printProductArrayToDom = (productArray) => {
+  for (let i = 0; i < productArray.length; i++) {
 
-    var currentProduct = productArray[i];
-    var productDomString = buildDomString(currentProduct);
+    const currentProduct = productArray[i];
+    const productDomString = buildDomString(currentProduct);
     productContainer.innerHTML += productDomString;
   }
 }
 
 printProductArrayToDom(allProducts);
 
+let selectedCard;
+
+document.getElementById("product-container").addEventListener("click", (e) => {
+    changeTheBorder(e);
+    printSelectedDescription();
+})
+
+const changeTheBorder = (e) => {
+    if (e.target.classList.contains("child")) {
+        selectedCard = e.target.parentNode;
+    } else if (e.target.parentNode.parentNode.classList.contains("product")) {
+        selectedCard = e.target.parentNode.parentNode;
+    } else if (e.target.classList.contains("product")) {
+        selectedCard = e.target;
+    }
+    selectedCard.classList.add("border-dash");
+    console.log("selectedCard", selectedCard);
+}
 
 
-
-
-
-
+const printSelectedDescription = () => {
+    let description = selectedCard.childNode[2].childNode[0].innerHTML;
+    console.log(description);
+}
 
 
 
